c3b9521d52f2a3461168e2e5cf1c915ea4ba6995
Tsirulnikov <PTsirulnikov@it-one.ru>
2024-01-09 14:22:50 +0300

    Generate category created

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
@@ -2,9 +2,12 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.GenerateCategory;
 import guru.qa.niffler.jupiter.GenerateSpend;
+import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
+import guru.qa.niffler.pages.MainPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,36 +22,34 @@ public class SpendingTest {
     Configuration.browserSize = "1980x1024";
   }
 
-  @BeforeEach
-  void doLogin() {
-    Selenide.open("http://127.0.0.1:3000/main");
-    $("a[href*='redirect']").click();
-    $("input[name='username']").setValue("duck");
-    $("input[name='password']").setValue("12345");
-    $("button[type='submit']").click();
-  }
+    @BeforeEach
+    void doLogin() {
+        Selenide.open("http://127.0.0.1:3000/main");
+        $("a[href*='redirect']").click();
+        $("input[name='username']").setValue("duck");
+        $("input[name='password']").setValue("1234");
+        $("button[type='submit']").click();
+    }
 
-  @GenerateSpend(
-      username = "duck",
-      description = "QA.GURU Advanced 4",
-      amount = 72500.00,
-      category = "Обучение",
-      currency = CurrencyValues.RUB
-  )
-  @Test
-  void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend) {
-    $(".spendings-table tbody")
-        .$$("tr")
-        .find(text(spend.description()))
-        .$$("td")
-        .first()
-        .click();
+    @GenerateCategory(
+            username = "duck",
+            category = "Обучение"
+    )
 
-    $(byText("Delete selected"))
-        .click();
+    @GenerateSpend(
+            username = "duck",
+            description = "QA.GURU Advanced 4",
+            amount = 72500.00,
+            category = "Обучение",
+            currency = CurrencyValues.RUB
+    )
+    @Test
+    void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend, CategoryJson category) {
 
-    $(".spendings-table tbody")
-        .$$("tr")
-        .shouldHave(size(0));
-  }
+        new MainPage()
+                .selectSpendingWithDescription(spend.description())
+                .clickDeleteSelectedButton()
+                .spendingRowsCountShouldBeEqualTo(0);
+
+    }
 }
